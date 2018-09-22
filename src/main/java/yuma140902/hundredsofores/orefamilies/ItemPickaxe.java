adf5c37c4ca58d5aa6a600ac9b98a34841f69506
yuma140902 <yuma140902@gmail.com>
2018-09-23 07:55:48 +0900

    ItemPickaxeクラスを実装

--- src/main/java/yuma140902/hundredsofores/orefamilies/ItemPickaxe.java
+++ src/main/java/yuma140902/hundredsofores/orefamilies/ItemPickaxe.java
@@ -1,45 +1,40 @@
-//package yuma140902.hundredsofores.orefamilies;
-//
-//import net.minecraftforge.common.util.EnumHelper;
-//import yuma140902.hundredsofores.orefamilies.core.OreFamilyMemberItemBase;
-//import yuma140902.hundredsofores.orefamilies.core.OreID;
-//
-//@Deprecated
-//public class ItemPickaxe extends OreFamilyMemberItemBase {
-//	public ItemPickaxe(String oreName) {
-//		this(new OreID(oreName));
-//	}
-//	
-//	public ItemPickaxe(OreID oreName) {
-//		super(oreName);
-//	}
-//
-//	@Override
-//	public String getItemUnlocalizedName() {
-//		// TODO 自動生成されたメソッド・スタブ
-//		return null;
-//	}
-//
-//	@Override
-//	public String getItemTextureName() {
-//		// TODO 自動生成されたメソッド・スタブ
-//		return null;
-//	}
-//
-//	@Override
-//	public String getOreDictionaryKey() {
-//		// TODO 自動生成されたメソッド・スタブ
-//		return null;
-//	}
-//	
-//	@Override
-//	public void register() {
-//		
-//		super.register();
-//	}
-//
-//	public void getToolMaterial() {
-//		EnumHelper.addToolMaterial(_oreName.getRawData()[0], 2, 128, 10.0F, 3, 0);//TODO new (test writing)
-//	}
-//	
-//}
+package yuma140902.hundredsofores.orefamilies;
+
+import javax.annotation.Nonnull;
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraftforge.oredict.OreDictionary;
+import yuma140902.hundredsofores.ModHundredsOfOres;
+import yuma140902.hundredsofores.orefamilies.core.IOreFamilyMember;
+import yuma140902.hundredsofores.orefamilies.core.OreID;
+import yuma140902.hundredsofores.util.StringUtil;
+
+public class ItemPickaxe extends net.minecraft.item.ItemPickaxe implements IOreFamilyMember {
+	
+	private OreID _oreName;
+	private @Nonnull String _oreDictKey = "pickaxe" + StringUtil.ToCase_XxxXxx(_oreName);
+	
+	public ItemPickaxe(String oreName, ToolMaterial material) {
+		this(new OreID(oreName), material);
+	}
+	
+	public ItemPickaxe(OreID oreName, ToolMaterial material) {
+		super(material);
+		_oreName = oreName;
+	}
+
+	@Override
+	public void register() {
+		this.setCreativeTab(ModHundredsOfOres.MOD_CREATIVETAB);
+		this.setCreativeTab(ModHundredsOfOres.MOD_CREATIVETAB);
+		this.setUnlocalizedName(ModHundredsOfOres.MOD_ID + "." + StringUtil.ToCase_xxx_xxx(_oreName) + "_pickaxe");
+		this.setTextureName(ModHundredsOfOres.MOD_ID + ":" + StringUtil.ToCase_xxx_xxx(_oreName) + "_pickaxe");
+		GameRegistry.registerItem(this, StringUtil.ToCase_xxx_xxx(_oreName) + "_pickaxe");
+		OreDictionary.registerOre(_oreDictKey, this);		
+	}
+
+	@Override
+	public String getOreDictionaryKey() {
+		return _oreDictKey;
+	}
+	
+}
