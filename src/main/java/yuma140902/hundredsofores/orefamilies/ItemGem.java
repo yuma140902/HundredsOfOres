7bfa29b9d8ae4692cdee6a77a301cf39f14e6273
yuma140902 <yuma140902@gmail.com>
2018-09-22 16:48:33 +0900

    ItemGemおよびOreFamilyWithGemを追加

---
+++ src/main/java/yuma140902/hundredsofores/orefamilies/ItemGem.java
@@ -0,0 +1,34 @@
+package yuma140902.hundredsofores.orefamilies;
+
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraftforge.oredict.OreDictionary;
+import yuma140902.hundredsofores.ModHundredsOfOres;
+import yuma140902.hundredsofores.orefamilies.core.OreFamilyMemberItemBase;
+import yuma140902.hundredsofores.orefamilies.core.OreID;
+import yuma140902.hundredsofores.util.StringUtil;
+
+public class ItemGem extends OreFamilyMemberItemBase {
+	public ItemGem(String oreName) {
+		this(new OreID(oreName));
+	}
+	
+	public ItemGem(OreID oreName) {
+		super(oreName);
+	}
+	
+	private final String _oreDictKey = "gem" + StringUtil.ToCase_XxxXxx(_oreName); 
+	@Override
+	public String getOreDictionaryKey() {
+		return _oreDictKey;
+	}
+	
+	@Override
+	public void register() {
+		this.setCreativeTab(ModHundredsOfOres.MOD_CREATIVETAB);
+		this.setCreativeTab(ModHundredsOfOres.MOD_CREATIVETAB);
+		this.setUnlocalizedName(ModHundredsOfOres.MOD_ID + "." + StringUtil.ToCase_xxx_xxx(_oreName) + "_gem");
+		this.setTextureName(ModHundredsOfOres.MOD_ID + ":" + StringUtil.ToCase_xxx_xxx(_oreName) + "_gem");
+		GameRegistry.registerItem(this, ModHundredsOfOres.MOD_ID + "." + StringUtil.ToCase_xxx_xxx(_oreName) + "_gem");
+		OreDictionary.registerOre(_oreDictKey, this);
+	}
+}
---
+++ src/main/java/yuma140902/hundredsofores/orefamilies/core/OreFamilyWithGem.java
@@ -0,0 +1,26 @@
+package yuma140902.hundredsofores.orefamilies.core;
+
+import yuma140902.hundredsofores.orefamilies.ItemGem;
+
+public class OreFamilyWithGem extends OreFamily {
+	public OreFamilyWithGem(String oreName) {
+		this(new OreID(oreName));
+	}
+	
+	public OreFamilyWithGem(OreID oreName) {
+		super(oreName);
+		this.itemGem = new ItemGem(oreName);
+	}
+	
+	@Override
+	public void register() {
+		super.register();
+		itemGem.register();
+	}
+	
+	protected ItemGem itemGem;
+	
+	public ItemGem getItemGem() {
+		return itemGem;
+	}
+}
