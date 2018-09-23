e730e8e3827a73656b7a0ec8a67c29ac3eeddc48
yuma140902 <yuma140902@gmail.com>
2018-09-23 09:42:21 +0900

    ハンマーを登録

--- src/main/java/yuma140902/hundredsofores/MyItems.java
+++ src/main/java/yuma140902/hundredsofores/MyItems.java
@@ -1,9 +1,13 @@
 package yuma140902.hundredsofores;
 
+import net.minecraft.item.Item;
+import yuma140902.hundredsofores.core.IRegisterable;
+import yuma140902.hundredsofores.items.Hummer;
+
 public final class MyItems {
 	public static void register() {
-// AItem.register();
+		((IRegisterable) hummer).register();
 	}
 	
-//public static final IMyItem AItem = ...;
+	public static final Item hummer = new Hummer();
 }
