7614ad6db444d17054fddbb1bf47b1cbc107c490
yuma140902 <yuma140902@gmail.com>
2018-09-23 19:42:16 +0900

    金の粉を追加

--- src/main/java/yuma140902/hundredsofores/MyItems.java
+++ src/main/java/yuma140902/hundredsofores/MyItems.java
@@ -3,11 +3,14 @@ package yuma140902.hundredsofores;
 import net.minecraft.item.Item;
 import yuma140902.hundredsofores.core.IRegisterable;
 import yuma140902.hundredsofores.items.Hummer;
+import yuma140902.hundredsofores.items.ItemDustGold;
 
 public final class MyItems {
 	public static void register() {
 		((IRegisterable) hummer).register();
+		((IRegisterable) goldDust).register();
 	}
 	
 	public static final Item hummer = new Hummer();
+	public static final Item goldDust = new ItemDustGold();
 }
---
+++ src/main/java/yuma140902/hundredsofores/items/ItemDustGold.java
@@ -0,0 +1,11 @@
+package yuma140902.hundredsofores.items;
+
+import yuma140902.hundredsofores.orefamilies.ItemDust;
+
+public class ItemDustGold extends ItemDust {
+
+	public ItemDustGold() {
+		super("gold");
+	}
+	
+}
