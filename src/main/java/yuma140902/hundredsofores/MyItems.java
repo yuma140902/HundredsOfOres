30eb8287e34bb2f8bd430ce85f32ceac8994c716
yuma140902 <yuma140902@gmail.com>
2018-09-23 19:47:16 +0900

    MyItemsのフィールドの型を変更

--- src/main/java/yuma140902/hundredsofores/MyItems.java
+++ src/main/java/yuma140902/hundredsofores/MyItems.java
@@ -1,16 +1,14 @@
 package yuma140902.hundredsofores;
 
-import net.minecraft.item.Item;
-import yuma140902.hundredsofores.core.IRegisterable;
 import yuma140902.hundredsofores.items.Hummer;
 import yuma140902.hundredsofores.items.ItemDustGold;
 
 public final class MyItems {
 	public static void register() {
-		((IRegisterable) hummer).register();
-		((IRegisterable) goldDust).register();
+		hummer.register();
+		goldDust.register();
 	}
 	
-	public static final Item hummer = new Hummer();
-	public static final Item goldDust = new ItemDustGold();
+	public static final Hummer hummer = new Hummer();
+	public static final ItemDustGold goldDust = new ItemDustGold();
 }
