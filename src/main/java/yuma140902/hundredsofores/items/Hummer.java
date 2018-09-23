6089bffb66da46bf3bfe7b31b0715b3d3f1efc4a
yuma140902 <yuma140902@gmail.com>
2018-09-23 12:28:54 +0900

    ハンマーがクラフト時に耐久値を消費してクラフトグリッドに残るようにした

--- src/main/java/yuma140902/hundredsofores/items/Hummer.java
+++ src/main/java/yuma140902/hundredsofores/items/Hummer.java
@@ -2,11 +2,19 @@ package yuma140902.hundredsofores.items;
 
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import yuma140902.hundredsofores.ModHundredsOfOres;
 import yuma140902.hundredsofores.core.IRegisterable;
 
 public class Hummer extends Item implements IRegisterable {
 
+	public Hummer() {
+		super();
+		setMaxStackSize(1);
+		setNoRepair();
+		setMaxDamage(127);
+	}
+	
 	@Override
 	public void register() {
 		this.setCreativeTab(ModHundredsOfOres.MOD_CREATIVETAB);
@@ -15,4 +23,21 @@ public class Hummer extends Item implements IRegisterable {
 		GameRegistry.registerItem(this, "hummer");
 	}
 	
+	@Override
+	public boolean doesContainerItemLeaveCraftingGrid(ItemStack p_77630_1_) {
+		return false;
+	}
+	
+	@Override
+	public ItemStack getContainerItem(ItemStack itemStack) {
+		if(itemStack != null && itemStack.getItem() == this) {
+			itemStack.setItemDamage(itemStack.getItemDamage() + 1);
+		}
+		return itemStack;
+	}
+	
+	@Override
+	public boolean hasContainerItem(ItemStack stack) {
+		return true;
+	}
 }
