66886c1c53cade6ac6628e99cdeca0376684f333
yuma140902 <yuma140902@gmail.com>
2018-09-23 20:07:00 +0900

    硫黄の追加

--- src/main/java/yuma140902/hundredsofores/MyItems.java
+++ src/main/java/yuma140902/hundredsofores/MyItems.java
@@ -2,13 +2,16 @@ package yuma140902.hundredsofores;
 
 import yuma140902.hundredsofores.items.Hummer;
 import yuma140902.hundredsofores.items.ItemDustGold;
+import yuma140902.hundredsofores.items.ItemDustSulfur;
 
 public final class MyItems {
 	public static void register() {
 		hummer.register();
 		goldDust.register();
+		sulferDust.register();
 	}
 	
 	public static final Hummer hummer = new Hummer();
 	public static final ItemDustGold goldDust = new ItemDustGold();
+	public static final ItemDustSulfur sulferDust = new ItemDustSulfur();
 }
---
+++ src/main/java/yuma140902/hundredsofores/items/ItemDustSulfur.java
@@ -0,0 +1,9 @@
+package yuma140902.hundredsofores.items;
+
+import yuma140902.hundredsofores.orefamilies.ItemDust;
+
+public class ItemDustSulfur extends ItemDust {
+	public ItemDustSulfur() {
+		super("sulfur");
+	}
+}
