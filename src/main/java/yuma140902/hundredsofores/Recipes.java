8d6a52a7b83ba7691bf5d15d39bdac84792fae71
yuma140902 <yuma140902@gmail.com>
2018-09-23 19:47:04 +0900

    金の粉砕レシピを追加

--- src/main/java/yuma140902/hundredsofores/Recipes.java
+++ src/main/java/yuma140902/hundredsofores/Recipes.java
@@ -1,8 +1,12 @@
 package yuma140902.hundredsofores;
 
 import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.oredict.OreDictionary;
 import net.minecraftforge.oredict.ShapedOreRecipe;
+import net.minecraftforge.oredict.ShapelessOreRecipe;
 
 public final class Recipes {
 	private Recipes() {}
@@ -24,7 +28,19 @@ public final class Recipes {
 	
 	// OreFamilyで登録されない粉(金の粉など)のレシピを追加します。
 	private static void registerDustRecipes() {
+		GameRegistry.addRecipe(new ShapelessOreRecipe(
+				new ItemStack(MyItems.goldDust, 2),
+				Blocks.gold_ore, new ItemStack(MyItems.hummer, 1, OreDictionary.WILDCARD_VALUE)));
+		GameRegistry.addRecipe(new ShapelessOreRecipe(
+				new ItemStack(MyItems.goldDust, 2),
+				"oreGold", new ItemStack(MyItems.hummer, 1, OreDictionary.WILDCARD_VALUE)));
 		
+		GameRegistry.addRecipe(new ShapelessOreRecipe(
+				MyItems.goldDust,
+				Items.gold_ingot, new ItemStack(MyItems.hummer, 1, OreDictionary.WILDCARD_VALUE)));
+		GameRegistry.addRecipe(new ShapelessOreRecipe(
+				MyItems.goldDust,
+				"ingotGold", new ItemStack(MyItems.hummer, 1, OreDictionary.WILDCARD_VALUE)));
 	}
 	
 	private static void registerAlloyRecipes() {
