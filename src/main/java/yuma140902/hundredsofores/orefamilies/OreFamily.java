3666c1379be293f1d857a53a35bd95cce1ee5098
yuma140902 <yuma140902@gmail.com>
2018-09-27 17:58:14 +0900

    featuresとexistFeaturesを用いて、より細かい指定ができるようになった(1)。また、setValues()を追加した(2)。
    (1) 例えば、「鉱石はないが、粉とインゴットは存在する」など。
    (2) setValues()はOreFamiliesよばれることを想定している

--- src/main/java/yuma140902/hundredsofores/orefamilies/OreFamily.java
+++ src/main/java/yuma140902/hundredsofores/orefamilies/OreFamily.java
@@ -1,10 +1,15 @@
 package yuma140902.hundredsofores.orefamilies;
 
+import java.util.EnumMap;
+import java.util.Map;
 import net.minecraftforge.common.config.Configuration;
 import yuma140902.hundredsofores.orefamilies.features.BlockCompressedBlock;
 import yuma140902.hundredsofores.orefamilies.features.BlockOre;
 import yuma140902.hundredsofores.orefamilies.features.ItemDust;
 import yuma140902.hundredsofores.orefamilies.features.ItemGear;
+import yuma140902.hundredsofores.orefamilies.features.ItemGem;
+import yuma140902.hundredsofores.orefamilies.features.ItemIngot;
+import yuma140902.hundredsofores.orefamilies.features.ItemNugget;
 import yuma140902.hundredsofores.recipes.RecipeRegisterHelper;
 import yuma140902.hundredsofores.worldGen.WorldGenerators;
 
@@ -19,10 +24,8 @@ public class OreFamily {
 	
 	private OreID _oreName;
 	
-	protected BlockOre blockOre;
-	protected ItemDust itemDust;
-	protected BlockCompressedBlock blockCompressedBlock;
-	protected ItemGear itemGear;
+	protected Map<FeatureType, IOreFamilyFeature> features = new EnumMap<>(FeatureType.class);
+	protected Map<FeatureType, Boolean> existFeatures = new EnumMap<>(FeatureType.class);
 	
 	public OreFamily(String oreName) {
 		this(new OreID(oreName));
@@ -30,16 +33,50 @@ public class OreFamily {
 	
 	public OreFamily(OreID oreName) {
 		this._oreName = oreName;
-		this.blockOre = new BlockOre(oreName);
-		this.itemDust = new ItemDust(oreName);
-		this.blockCompressedBlock = new BlockCompressedBlock(oreName);
-		this.itemGear = new ItemGear(oreName);
+		
+		for(FeatureType key : FeatureType.values()) {
+			features.put(key, IOreFamilyFeature.EMPTY);
+		}
+	}
+	
+	// existFeaturesの値に基づいてfeaturesを初期化します。(ただしpickaxeは除く)
+	public void setValues() {
+		for(Map.Entry<FeatureType, Boolean> entry : existFeatures.entrySet()) {
+			if(entry.getValue() == true) {
+				switch (entry.getKey()) {
+					case ORE:
+						features.put(FeatureType.ORE, new BlockOre(_oreName));
+						break;
+					case BLOCK:
+						features.put(FeatureType.BLOCK, new BlockCompressedBlock(_oreName));
+						break;
+					case INGOT:
+						features.put(FeatureType.INGOT, new ItemIngot(_oreName));
+						break;
+					case NUGGET:
+						features.put(FeatureType.NUGGET, new ItemNugget(_oreName));
+						break;
+					case GEM:
+						features.put(FeatureType.GEM, new ItemGem(_oreName));
+						break;
+					case DUST:
+						features.put(FeatureType.DUST, new ItemDust(_oreName));
+						break;
+					case GEAR:
+						features.put(FeatureType.GEAR, new ItemGear(_oreName));
+						break;
+					case PICKAXE:
+//						features.put(FeatureType.PICKAXE, new ItemPickaxe());
+						break;
+				}
+			}
+		}
 	}
 	
 	public void init() {
 		// レシピの追加など
-		BlockOre ore = this.getBlockOre();
-		ItemDust dust = this.getItemDust();
+		BlockOre ore = (BlockOre) getFeature(FeatureType.ORE);
+		ItemDust dust = (ItemDust) getFeature(FeatureType.DUST);
 		
 		String oreOredict = ore.getOreDictionaryKey();
 		
@@ -49,18 +86,18 @@ public class OreFamily {
 	}
 	
 	public void register() {
-		this.getBlockOre().register();
-		this.getItemDust().register();
-		this.getBlockCompressedIngot().register();
-		this.getItemGear().register();
+		getFeature(FeatureType.ORE).register();
+		getFeature(FeatureType.DUST).register();
+		getFeature(FeatureType.BLOCK).register();
+		getFeature(FeatureType.GEAR).register();
 	}
 	
 	public void loadOreGenConfig(Configuration cfg) {
-		this.getBlockOre().loadConfig(cfg);
+		((BlockOre) getFeature(FeatureType.ORE)).loadConfig(cfg);
 	}
 	
 	public void registerToWorldGenerators() {
-		BlockOre ore = this.getBlockOre();
+		BlockOre ore = (BlockOre) getFeature(FeatureType.ORE);
 		WorldGenerators.oreGenerator.addOreGenerator(ore, ore.getOreGeneratorConfig());
 	}
 	
@@ -68,20 +105,20 @@ public class OreFamily {
 		return this._oreName;
 	}
 	
-	public BlockOre getBlockOre() {
-		return blockOre;
+	public void includeFeature(FeatureType featureType) {
+		existFeatures.put(featureType, true);
 	}
 	
-	public ItemDust getItemDust() {
-		return itemDust;
+	public void excludeFeature(FeatureType featureType) {
+		existFeatures.put(featureType, false);
 	}
 	
-	public BlockCompressedBlock getBlockCompressedIngot() {
-		return blockCompressedBlock;
+	public boolean hasFeature(FeatureType featureType) {
+		return existFeatures.get(featureType);
 	}
 	
-	public ItemGear getItemGear() {
-		return itemGear;
+	public IOreFamilyFeature getFeature(FeatureType featureType) {
+		return features.get(featureType);
 	}
 	
 }
