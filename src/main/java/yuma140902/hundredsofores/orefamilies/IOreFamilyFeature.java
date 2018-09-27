624ee8972d5ea31e936fa0c7318c007be33780bd
yuma140902 <yuma140902@gmail.com>
2018-09-27 17:56:19 +0900

    EMPTY定数を追加。nullの代わりにこれを返すことを想定

--- src/main/java/yuma140902/hundredsofores/orefamilies/IOreFamilyFeature.java
+++ src/main/java/yuma140902/hundredsofores/orefamilies/IOreFamilyFeature.java
@@ -4,5 +4,20 @@ import yuma140902.hundredsofores.core.IOreDictionary;
 import yuma140902.hundredsofores.core.IRegisterable;
 
 public interface IOreFamilyFeature extends IRegisterable, IOreDictionary {
+	public static final IOreFamilyFeature EMPTY = new IOreFamilyFeature() {
+		
+		@Override
+		public String getOreDictionaryKey() {
+			return null;
+		}
+		
+		@Override
+		public void register() {
+			
+		}
+	};
 	
+	public static boolean isEmpty(IOreFamilyFeature obj) {
+		return EMPTY.equals(obj);
+	}
 }
