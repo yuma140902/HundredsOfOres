a85d6f35e1cac05680e8ded176212d4d13736dc0
yuma140902 <yuma140902@gmail.com>
2018-09-27 18:46:28 +0900

    クリエイティブタブのアイコンを銅鉱石に変更

--- src/main/java/yuma140902/hundredsofores/creativeTabs/HOOCreativeTab.java
+++ src/main/java/yuma140902/hundredsofores/creativeTabs/HOOCreativeTab.java
@@ -2,9 +2,11 @@ package yuma140902.hundredsofores.creativeTabs;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import yuma140902.hundredsofores.orefamilies.FeatureType;
 import yuma140902.hundredsofores.orefamilies.OreFamilies;
 
 public class HOOCreativeTab extends CreativeTabs {
@@ -15,7 +17,7 @@ public class HOOCreativeTab extends CreativeTabs {
 	@Override
 	@SideOnly(Side.CLIENT)
 	public ItemStack getIconItemStack() {
-		return new ItemStack(OreFamilies.tin.getBlockOre());
+		return new ItemStack((Block) OreFamilies.copper.getFeature(FeatureType.ORE));
 	}
 	
 	@Override
