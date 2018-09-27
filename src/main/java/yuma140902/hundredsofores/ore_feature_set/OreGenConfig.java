7a6e514cd05d88e54d4d83f07b4920f3419a04b0
yuma140902 <yuma140902@gmail.com>
2018-09-27 20:55:18 +0900

    仕様変更。Configファイルにおけるデフォルト値も格納するようになった

--- src/main/java/yuma140902/hundredsofores/ore_feature_set/OreGenConfig.java
+++ src/main/java/yuma140902/hundredsofores/ore_feature_set/OreGenConfig.java
@@ -1,7 +1,14 @@
 package yuma140902.hundredsofores.ore_feature_set;
 
+import yuma140902.hundredsofores.util.ListUtil;
+
 public class OreGenConfig {
+	public OreGenConfig() {
+		updateDefault();
+	}
+	
 	public OreGenConfig(int spawnTries, int spawnSize, int maxHeight, int minHeight, int... dimensionBlackList) {
+		this.enabled = true;
 		this.spawnTries = spawnTries;
 		this.spawnSize = spawnSize;
 		this.maxHeight = maxHeight;
@@ -9,6 +16,7 @@ public class OreGenConfig {
 		this.dimensionBlackList = dimensionBlackList;
 	}
 	
+	public boolean enabled;
 	public int spawnTries;
 	public int spawnSize;
 	public int maxHeight;
@@ -16,4 +24,46 @@ public class OreGenConfig {
 	public int[] dimensionBlackList;
 	
 	public int heightDiff; //この項目はキャッシュとして使用されます。
+	
+	
+	
+	protected boolean defaultIsOrdinaryGenEnabled = true;
+	protected int defaultSpawnTries = 20;
+	protected int defaultSpawnSize = 8;
+	protected int defaultMaxHeight = 64;
+	protected int defaultMinHeight = 0;
+	protected String[] defaultDimensionBlackListStr = new String[] {};
+	
+	public void updateDefault() {
+		this.enabled = defaultIsOrdinaryGenEnabled;
+		this.spawnTries = defaultSpawnTries;
+		this.spawnSize = defaultSpawnSize;
+		this.maxHeight = defaultMaxHeight;
+		this.minHeight = defaultMinHeight;
+		this.dimensionBlackList = ListUtil.ToIntList(defaultDimensionBlackListStr);
+	}
+	
+	public void setdefaultIsOrdinaryGenEnabled(boolean value) {
+		this.defaultIsOrdinaryGenEnabled = value;
+	}
+	
+	public void setdefaultSpawnTries(int value) {
+		this.defaultSpawnTries = value;
+	}
+	
+	public void setdefaultSpawnSize(int value) {
+		this.defaultSpawnSize = value;
+	}
+	
+	public void setdefaultMaxHeight(int value) {
+		this.defaultMaxHeight = value;
+	}
+	
+	public void setdefaultMinHeight(int value) {
+		this.defaultMinHeight = value;
+	}
+	
+	public void setdefaultDimensionBlackListStr(String[] value) {
+		this.defaultDimensionBlackListStr = value;
+	}
 }
